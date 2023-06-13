e622f7400762cd109ce97559c97d98816b370e2b
Yavor Krastev <ykr@sitecore.net>
2023-06-13 17:16:18 +0300

    shopping cart and application user repositories

---
+++ Bulky.DataAccess/Repository/ApplicationUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Bulky.Models;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class ApplicationUserRepository : Repository<ApplicationUser>, IApplicationUserRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public ApplicationUserRepository(ApplicationDbContext db) : base(db)
+        {
+            this._db = db;
+        }
+    }
+}
--- Bulky.Models/ShoppingCart.cs
+++ Bulky.Models/ShoppingCart.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Bulky.Models
 {
-    internal class ShoppingCart
+    public class ShoppingCart
     {
+        public int Id { get; set; }
+
+        public int ProductId { get; set; }
+
+        [ValidateNever]
+        [ForeignKey("ProductId")]
+        public Product Product { get; set; }
+
+        [Range(1, 1000, ErrorMessage = "Should be value between 1 and 1000")]
+        public int Count { get; set; }
+
+        public string ApplicationUserId { get; set; }
+
+        [ValidateNever]
+        [ForeignKey("ApplicationUserId")]
+        public ApplicationUser ApplicationUser { get; set; }
     }
 }
